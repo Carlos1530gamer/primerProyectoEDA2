09363098770fa9a2ea60510c2cd02573ac7598af
Carlos Daniel Hernandez Chauteco <gamercarlos1530@comunidad.unam.mx>
2019-03-06 22:22:29 -0600

    agregado el proyecto de eda creado en el IDE NetBeams

---
+++ EDA2Proyecto1Eq#G5/src/eda2proyecto1eq/g5/EDA2Proyecto1EqG5.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eda2proyecto1eq.g5;
+
+/**
+ *
+ * @author scoop
+ */
+public class EDA2Proyecto1EqG5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
